fce3d733e1ad824727ea4eee7afa6af136044919
The Lucky Clover <pounds1@gmail.com>
2025-07-27 13:41:12 -0700

    Create file.py

---
+++ agentcy-backend/src/models/file.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from src.models import db
+
+class File(db.Model):
+    __tablename__ = 'files'
+
+    id = db.Column(db.Integer, primary_key=True)
+    conversation_id = db.Column(db.String(100), nullable=False, index=True)
+    filename = db.Column(db.String(255), nullable=False)
+    filepath = db.Column(db.String(500), nullable=False)
+    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "conversation_id": self.conversation_id,
+            "filename": self.filename,
+            "filepath": self.filepath,
+            "uploaded_at": self.uploaded_at.isoformat() if self.uploaded_at else None
+        }
+
+    def __repr__(self):
+        return f"<File {self.id} {self.filename}>"
