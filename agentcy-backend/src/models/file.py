37eb36cdefb9fccdee7ff129c712af0edaecd6ac
The Lucky Clover <pounds1@gmail.com>
2025-07-27 15:08:21 -0700

    Update file.py

--- agentcy-backend/src/models/file.py
+++ agentcy-backend/src/models/file.py
@@ -5,19 +5,18 @@ class File(db.Model):
     __tablename__ = 'files'
 
     id = db.Column(db.Integer, primary_key=True)
-    conversation_id = db.Column(db.String(100), db.ForeignKey('conversations.conversation_id'), nullable=False, index=True)
     filename = db.Column(db.String(255), nullable=False)
-    filepath = db.Column(db.String(500), nullable=False)
-    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    filepath = db.Column(db.String(1024), nullable=False)
+    conversation_id = db.Column(db.String(100), db.ForeignKey('conversations.conversation_id'), nullable=True)
+    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
         return {
-            "id": self.id,
-            "conversation_id": self.conversation_id,
-            "filename": self.filename,
-            "filepath": self.filepath,
-            "uploaded_at": self.uploaded_at.isoformat() if self.uploaded_at else None
+            'id': self.id,
+            'filename': self.filename,
+            'filepath': self.filepath,
+            'uploaded_at': self.uploaded_at.isoformat() if self.uploaded_at else None
         }
 
     def __repr__(self):
-        return f'<File {self.id} - {self.filename}>'
+        return f'<File {self.id}: {self.filename}>'
